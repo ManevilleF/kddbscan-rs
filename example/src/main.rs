97cc83e6a21f7ef17192b39ca9889a527db3408c
Félix Lescaudey de Maneville <felix.maneville@gmail.com>
2024-06-19 15:20:16 +0200

    Cleaned example

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,5 +1,5 @@
-use kddbscan::{cluster, IntoPoint, ClusterId};
-use colored::{Colorize};
+use colored::Colorize;
+use kddbscan::{cluster, ClusterId, IntoPoint};
 
 pub struct Coordinate {
     pub x: f64,
@@ -13,52 +13,52 @@ impl IntoPoint for Coordinate {
 }
 
 fn main() {
-    let mut coordinates: Vec<Coordinate> = vec![];
-    coordinates.push(Coordinate { x: 11.0, y: 12.0 });
-    coordinates.push(Coordinate { x: 0.0, y: 0.0 });
-    coordinates.push(Coordinate { x: 12.0, y: 11.0 });
-    coordinates.push(Coordinate { x: 11.0, y: 9.0 });
-    coordinates.push(Coordinate { x: 10.0, y: 8.0 });
-    coordinates.push(Coordinate { x: 1.0, y: 2.0 });
-    coordinates.push(Coordinate { x: 3.0, y: 1.0 });
-    coordinates.push(Coordinate { x: 4.0, y: 4.0 });
-    coordinates.push(Coordinate { x: 9.0, y: 0.0 });
+    let coordinates: Vec<Coordinate> = vec![
+        Coordinate { x: 11.0, y: 12.0 },
+        Coordinate { x: 0.0, y: 0.0 },
+        Coordinate { x: 12.0, y: 11.0 },
+        Coordinate { x: 11.0, y: 9.0 },
+        Coordinate { x: 10.0, y: 8.0 },
+        Coordinate { x: 1.0, y: 2.0 },
+        Coordinate { x: 3.0, y: 1.0 },
+        Coordinate { x: 4.0, y: 4.0 },
+        Coordinate { x: 9.0, y: 0.0 },
+    ];
 
-    let clustered =  cluster(coordinates, 2, None, None);
+    let clustered = cluster(coordinates, 2, None, None);
 
     println!("  _______________        _______________");
     println!("  |          111|        |          111|");
     println!("  |0123456789012|        |0123456789012|");
     println!("  |-------------|        |-------------|");
     for y in 0..13 {
-        
         for x in 0..13 {
-            let item = clustered.iter().find(|w|{w.into_inner().x==(x as f64)&&w.into_inner().y==(y as f64)});
+            let item = clustered
+                .iter()
+                .find(|w| w.into_inner().x == (x as f64) && w.into_inner().y == (y as f64));
 
             let point = match item {
-                Some(_)=>{
-                    "X".black()
-                }
-                None=>{
-                    " ".white()
-                }
+                Some(_) => "X".black(),
+                None => " ".white(),
             };
 
-            if x==0 {
-                print!("{: <2}|",y);
-            } 
+            if x == 0 {
+                print!("{: <2}|", y);
+            }
             print!("{}", point);
         }
 
-        print!("|{: <6}","");
+        print!("|{: <6}", "");
 
         for x in 0..13 {
-            let item = clustered.iter().find(|w|{w.into_inner().x==(x as f64)&&w.into_inner().y==(y as f64)});
+            let item = clustered
+                .iter()
+                .find(|w| w.into_inner().x == (x as f64) && w.into_inner().y == (y as f64));
 
             let point = match item {
-                Some(item)=>{
-                    if let ClusterId::Classified(cid)  = item.get_cluster_id() {
-                        if cid==&0 {
+                Some(item) => {
+                    if let ClusterId::Classified(cid) = item.get_cluster_id() {
+                        if cid == &0 {
                             "X".blue()
                         } else {
                             "X".green()
@@ -67,20 +67,17 @@ fn main() {
                         "X".red()
                     }
                 }
-                None=>{
-                    " ".white()
-                }
+                None => " ".white(),
             };
 
-            if x==12 {
-                println!("{}|",point);
+            if x == 12 {
+                println!("{}|", point);
             } else {
-                if x==0 {
-                    print!("{: <2}|",y);
+                if x == 0 {
+                    print!("{: <2}|", y);
                 }
                 print!("{}", point);
             }
-
         }
     }
 
